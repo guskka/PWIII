9d5378b05eaa5f4753ed77b9e888b6df007b529e
guskka <gustavo.guska.goncalves@gmail.com>
2026-03-19 15:13:30 -0300

    style: ajusta padding nos dropdowns para consistência visual feat: oculta operações de bhaskara quando inputs estão vazios

--- 2 - Operacoes Matematicas/src/js/features/bhaskara.js
+++ 2 - Operacoes Matematicas/src/js/features/bhaskara.js
@@ -11,6 +11,24 @@ resX2.value = 0;
 
 document.getElementById("operacoesBhaskara").style.display = "none";
 
+numA.addEventListener("input", () => {
+    if (numA.value === "") {
+        document.getElementById("operacoesBhaskara").style.display = "none";
+    }
+});
+
+numB.addEventListener("input", () => {
+    if (numB.value === "") {
+        document.getElementById("operacoesBhaskara").style.display = "none";
+    }
+});
+
+numC.addEventListener("input", () => {
+    if (numC.value === "") {
+        document.getElementById("operacoesBhaskara").style.display = "none";
+    }
+});
+
 function calcBhaskara() {
     if (numA.value != "" || numB.value != "" || numC.value != "") {
         document.getElementById("operacoesBhaskara").style.display = "flex";
@@ -21,4 +39,13 @@ function calcBhaskara() {
     const valorC = numC.value;
 
     resDelta.value = (valorB ** 2) - 4 * valorA * valorC;
+
+    if (resDelta.value < 0) {
+        resX1.value = "Não existe raiz real";
+        resX2.value = "Não existe raiz real";
+    }
+    else {
+        resX1.value = (-valorB + Math.sqrt(resDelta.value)) / (2 * valorA);
+        resX2.value = (-valorB - Math.sqrt(resDelta.value)) / (2 * valorA);
+    }
 };
