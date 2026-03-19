bef4ec6fed9cc091d8467a4604964ff55617020e
guskka <gustavo.guska.goncalves@gmail.com>
2026-03-19 12:46:55 -0300

    style: oculta resultados quando input vazio em multiplicações

--- 2 - Operacoes Matematicas/src/js/features/multiplicacao.js
+++ 2 - Operacoes Matematicas/src/js/features/multiplicacao.js
@@ -11,8 +11,23 @@ resQuadruplo.value = 0;
 resQuintuplo.value = 0;
 resSextuplo.value = 0;
 
+document.getElementById("operacoesMultiplicacao").style.display = "none";
+
 numMultiplicacao.addEventListener("input", () => {
+    if (numMultiplicacao.value != "") {
+        document.getElementById("operacoesMultiplicacao").style.display = "flex";
+    }
+    else {
+        resDobro.value = 0;
+        resTriplo.value = 0;
+        resQuadruplo.value = 0;
+        resQuintuplo.value = 0;
+        resSextuplo.value = 0;
+        document.getElementById("operacoesMultiplicacao").style.display = "none";
+    }
+
     const valor = numMultiplicacao.value;
+
 
     resDobro.value = valor * 2;
     resTriplo.value = valor * 3;
