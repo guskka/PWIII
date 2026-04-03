6617025790bfd9d925ff18b5af8272b4cc48cb31
guskka <gustavo.guska.goncalves@gmail.com>
2026-04-03 20:45:43 -0300

    feat: implement product and payment sections with navigation functionality

---
+++ 3 - Loja Controle Vendas/src/components/navbar.js
@@ -0,0 +1,14 @@
+const secProdutos = document.getElementById("secProdutos");
+const secPagamento = document.getElementById("secPagamento");
+
+secProdutos.style.display = "none";
+
+function showPagamento() {
+  secProdutos.style.display = "none";
+  secPagamento.style.display = "flex";
+}
+
+function showProdutos() {
+  secPagamento.style.display = "none";
+  secProdutos.style.display = "flex";
+}
